7e30cfef7e83aba8978fd6b70cf796fdee4c2651
denghongjie <153010565+sakura64605@users.noreply.github.com>
2026-04-20 16:53:57 +0800

    缺陷修复

--- PMS-vue/src/views/private-message/index.vue
+++ PMS-vue/src/views/private-message/index.vue
@@ -491,18 +491,12 @@ onMounted(async () => {
 <style scoped>
 .private-message-container {
   display: flex;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   background-color: #f5f7fa;
   overflow: hidden;
   margin: 0;
   padding: 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 9999;
   box-sizing: border-box;
 }
 
